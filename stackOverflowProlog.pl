8bb37bd3b274746aea27faae972a9bf0ba0b521d
Israel <iariaspanez@gmail.com>
2020-11-24 17:57:30 -0300

    Implementación de TDA respuesta.

--- stackOverflowProlog.pl
+++ stackOverflowProlog.pl
@@ -1,12 +1,12 @@
 /*
 
-███████╗████████╗ █████╗  ██████╗██╗  ██╗    ████████╗██████╗  █████╗ 
-██╔════╝╚══██╔══╝██╔══██╗██╔════╝██║ ██╔╝    ╚══██╔══╝██╔══██╗██╔══██╗
-███████╗   ██║   ███████║██║     █████╔╝        ██║   ██║  ██║███████║
-╚════██║   ██║   ██╔══██║██║     ██╔═██╗        ██║   ██║  ██║██╔══██║
-███████║   ██║   ██║  ██║╚██████╗██║  ██╗       ██║   ██████╔╝██║  ██║
-╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝       ╚═╝   ╚═════╝ ╚═╝  ╚═╝
- 
+████████╗██████╗  █████╗     ███████╗████████╗ █████╗  ██████╗██╗  ██╗
+╚══██╔══╝██╔══██╗██╔══██╗    ██╔════╝╚══██╔══╝██╔══██╗██╔════╝██║ ██╔╝
+   ██║   ██║  ██║███████║    ███████╗   ██║   ███████║██║     █████╔╝ 
+   ██║   ██║  ██║██╔══██║    ╚════██║   ██║   ██╔══██║██║     ██╔═██╗ 
+   ██║   ██████╔╝██║  ██║    ███████║   ██║   ██║  ██║╚██████╗██║  ██╗
+   ╚═╝   ╚═════╝ ╚═╝  ╚═╝    ╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝
+                                                                      
  */
 
 % El tda Stack es la base para este programa, por ende, es posible requiera predicados de los otros tda.
@@ -41,7 +41,7 @@ stackGetActiveUser([_,_,_,ActiveUser], ActiveUser).
 crearPregunta(Pregunta,Id,Autor,Fecha,Etiquetas,EstadoPregunta,Recompensa,VotosTotales,SalidaPregunta):-
     SalidaPregunta = [Pregunta, Id, Autor, Fecha, Etiquetas, EstadoPregunta, Recompensa, VotosTotales].
 
-%Selectores
+% Selectores
 
 questionGetPregunta([Question,_,_,_,_,_,_,_], Question).
 
@@ -58,6 +58,72 @@ questionGetStatus([_,_,_,_,_,Status,_,_], Status).
 questionGetRecompensa([_,_,_,_,_,_,Reward,_], Reward).
 
 questionGetVotes([_,_,_,_,_,_,_,Votes], Votes).
+
+/*
+████████╗██████╗  █████╗     ██████╗ ███████╗███████╗██████╗ ██╗   ██╗███████╗███████╗████████╗ █████╗ 
+╚══██╔══╝██╔══██╗██╔══██╗    ██╔══██╗██╔════╝██╔════╝██╔══██╗██║   ██║██╔════╝██╔════╝╚══██╔══╝██╔══██╗
+   ██║   ██║  ██║███████║    ██████╔╝█████╗  ███████╗██████╔╝██║   ██║█████╗  ███████╗   ██║   ███████║
+   ██║   ██║  ██║██╔══██║    ██╔══██╗██╔══╝  ╚════██║██╔═══╝ ██║   ██║██╔══╝  ╚════██║   ██║   ██╔══██║
+   ██║   ██████╔╝██║  ██║    ██║  ██║███████╗███████║██║     ╚██████╔╝███████╗███████║   ██║   ██║  ██║
+   ╚═╝   ╚═════╝ ╚═╝  ╚═╝    ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝      ╚═════╝ ╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═╝
+*/                                                                                                       
+
+%Representación
+
+% (string X entero X entero X string X date X labels X string X entero)
+% [respuesta, idPreguntaQueResponde, idRespuesta, autor, [fecha], [etiquetas], estadoRespuesta, votosTotales]
+
+% Constructor
+
+crearRespuesta(Respuesta,IdPreguntaQueResponde,IdRespuesta,Autor,Fecha,Etiquetas,EstadoRespuesta,VotosTotales,SalidaRespuesta):-
+    SalidaRespuesta = [Respuesta,IdPreguntaQueResponde,IdRespuesta,Autor,Fecha,Etiquetas,EstadoRespuesta,VotosTotales].
+
+% Selectores
+
+answerGetRespuesta([Respuesta,_,_,_,_,_,_,_], Respuesta).
+
+answerGetIdPregunta([_,IdPreguntaQueResponde,_,_,_,_,_,_], IdPreguntaQueResponde).
+
+answerGetId([_,_,IdRespuesta,_,_,_,_,_], IdRespuesta).
+
+answerGetUser([_,_,_,Autor,_,_,_,_], Autor).
+
+answerGetDate([_,_,_,_,Fecha,_,_,_], Fecha).
+
+answerGetLabels([_,_,_,_,_,Etiquetas,_,_], Etiquetas).
+
+answerGetStatus([_,_,_,_,_,_,EstadoRespuesta,_], EstadoRespuesta).
+
+answerGetVotes([_,_,_,_,_,_,_,VotosTotales], VotosTotales).
+
+
+/*
+████████╗██████╗  █████╗     ██╗   ██╗███████╗██╗   ██╗ █████╗ ██████╗ ██╗ ██████╗ ███████╗
+╚══██╔══╝██╔══██╗██╔══██╗    ██║   ██║██╔════╝██║   ██║██╔══██╗██╔══██╗██║██╔═══██╗██╔════╝
+   ██║   ██║  ██║███████║    ██║   ██║███████╗██║   ██║███████║██████╔╝██║██║   ██║███████╗
+   ██║   ██║  ██║██╔══██║    ██║   ██║╚════██║██║   ██║██╔══██║██╔══██╗██║██║   ██║╚════██║
+   ██║   ██████╔╝██║  ██║    ╚██████╔╝███████║╚██████╔╝██║  ██║██║  ██║██║╚██████╔╝███████║
+   ╚═╝   ╚═════╝ ╚═╝  ╚═╝     ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝ ╚═════╝ ╚══════╝
+*/
+
+% Representacion
+% (string X string X(list entero)X entero)
+% [usuario password [preguntas] reputacion]
+
+% Constructor
+
+crearUsuario(Username,Password,Preguntas,Reputacion,SalidaUsuario):-
+    SalidaUsuario = [Username,Password,Preguntas,Reputacion].
+
+% Selectores
+
+userGetUsername([Username,_,_,_], Username).
+
+userGetPassword([Username,_,_,_], Username).
+
+userGetQuestions([_,_,Questions,_], Question).
+
+userGetReputation([_,_,_,Reputacion], Reputacion).
 
 /*
 ██████╗  █████╗ ███████╗███████╗    ██████╗ ███████╗    ██╗  ██╗███████╗ ██████╗██╗  ██╗ ██████╗ ███████╗
